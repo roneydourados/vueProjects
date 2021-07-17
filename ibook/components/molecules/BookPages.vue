f4243db6dba45ebe38f8b338a5b2bfb830236c4a
Roney Melo <roneydourados@gmail.com>
2021-07-17 18:57:23 -0400

    criação da pagina de leitura

---
+++ ibook/components/molecules/BookPages.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="book-pages">
+    <h2>{{ $book.title }}</h2>
+    <BookPage
+      v-for="page in $pages"
+      :key="page.id"
+      :page-number="page.pageNumber"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { books } from '@/store'
+
+export default Vue.extend({
+  computed: {
+    $pages() {
+      return books.$single.pages
+    },
+
+    $book() {
+      return books.$single
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ ibook/pages/book/_id/read.vue
@@ -0,0 +1,27 @@
+<template>
+  <BookReadTemplate />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { books } from '@/store'
+
+export default Vue.extend({
+  layout: 'ibook',
+  async asyncData({ params }) {
+    await books.show({ id: Number(params.id) })
+  },
+  head() {
+    return {
+      title: books.$single.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: books.$single.title
+        }
+      ]
+    }
+  }
+})
+</script>
